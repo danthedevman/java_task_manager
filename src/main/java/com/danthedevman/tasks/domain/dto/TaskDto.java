72f9eb327b5a49522f95d58b0fdac73d8ef2d224
danthedevman <dev.danielpalmer@gmail.com>
2025-12-28 17:54:21 -0500

    added task and task list dtos

---
+++ src/main/java/com/danthedevman/tasks/domain/dto/TaskDto.java
@@ -0,0 +1,18 @@
+package com.danthedevman.tasks.domain.dto;
+
+import com.danthedevman.tasks.domain.entities.TaskPriority;
+import com.danthedevman.tasks.domain.entities.TaskStatus;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TaskDto(
+        UUID id,
+        String title,
+        String description,
+        LocalDateTime dueDate,
+        TaskPriority priority,
+        TaskStatus status
+) {
+
+}
