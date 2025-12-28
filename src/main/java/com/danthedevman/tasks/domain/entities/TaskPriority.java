3a238cfd927a4de956511311d5e47eeb56742da3
danthedevman <dev.danielpalmer@gmail.com>
2025-12-28 15:21:41 -0500

    init commit

---
+++ src/main/java/com/danthedevman/tasks/domain/entities/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.danthedevman.tasks.domain.entities;
+
+public enum TaskPriority {
+    HIGH,MEDIUM,LOW
+}
---
+++ src/main/java/com/danthedevman/tasks/domain/entities/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.danthedevman.tasks.domain.entities;
+
+public enum TaskStatus {
+    OPEN,CLOSED
+}
