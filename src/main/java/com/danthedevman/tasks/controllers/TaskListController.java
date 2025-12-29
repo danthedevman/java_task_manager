6301323a737d298a37cfe232dc7be2963b8e1bb1
danthedevman <dev.danielpalmer@gmail.com>
2025-12-29 15:41:43 -0500

    added post method for creating task lists

--- src/main/java/com/danthedevman/tasks/controllers/TaskListController.java
+++ src/main/java/com/danthedevman/tasks/controllers/TaskListController.java
@@ -1,11 +1,10 @@
 package com.danthedevman.tasks.controllers;
 
 import com.danthedevman.tasks.domain.dto.TaskListDto;
+import com.danthedevman.tasks.domain.entities.TaskList;
 import com.danthedevman.tasks.mappers.TaskListMapper;
 import com.danthedevman.tasks.services.TaskListService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,12 @@ public class TaskListController {
     @GetMapping
     public List<TaskListDto> listTaskLists(){
         return taskListService.listTaskLists().stream().map(taskListMapper::toDto).toList();
+    }
+
+    @PostMapping
+    public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto){
+    TaskList createdTaskList = taskListService.createTaskList(taskListMapper.fromDto(taskListDto));
+
+    return taskListMapper.toDto(createdTaskList);
     }
 }
