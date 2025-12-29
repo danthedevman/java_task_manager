55ea4df78275c3c6c3122fcc7eee691a4249d69d
danthedevman <dev.danielpalmer@gmail.com>
2025-12-29 18:02:00 -0500

    added delete method for task lists

--- src/main/java/com/danthedevman/tasks/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/danthedevman/tasks/services/impl/TaskListServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -48,5 +49,30 @@ public class TaskListServiceImpl  implements TaskListService {
     @Override
     public Optional<TaskList> getTaskList(UUID id) {
         return taskListRepository.findById(id);
+    }
+
+    @Override
+    public TaskList updateTaskList(UUID taskListId, TaskList taskList) {
+        if(null == taskList.getId()){
+            throw new IllegalArgumentException("Task list must have an id!");
+        }
+
+        if(!Objects.equals(taskList.getId(),taskListId)){
+            throw new IllegalArgumentException("Cannot change task list id!");
+        }
+
+       TaskList existingTaskList = taskListRepository.findById(taskListId).orElseThrow(()->{
+            throw new IllegalArgumentException("Task list not found!");
+        });
+
+        existingTaskList.setTitle(taskList.getTitle());
+        existingTaskList.setDescription(taskList.getDescription());
+        existingTaskList.setUpdated(LocalDateTime.now());
+        return taskListRepository.save(existingTaskList);
+    }
+
+    @Override
+    public void deleteTaskList(UUID taskListId) {
+        taskListRepository.deleteById(taskListId);
     }
 }
